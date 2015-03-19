6221713e0ece49c9da8e513ef206bf800fe7ccc2
yhc <yuhongchun027@gmail.com>
2015-03-19 14:45:42 +0800

    first commit

---
+++ ssh_hostsdeny-v2.0.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+awk '{for(i=1;i<=NF;i++){if($i ~ /rhost/)print substr($i,7)}}' /var/log/secure | sort | uniq    -c>/root/black.txt
+DEFINE="100"
+cat     /root/black.txt |    while read LINE
+do
+               NUM=`echo $LINE |awk '{print $1}'`
+               host=`echo $LINE    |awk '{print $2}'`
+               if [ $NUM -gt $DEFINE ];
+               then
+                grep $host    /etc/hosts.deny > /dev/null
+                   if [ $? -gt 0 ];
+                   then
+                   echo "sshd:$host"     >> /etc/hosts.deny
+                   echo "vsftpd:$host" >> /etc/hosts.deny
+                   fi
+               fi
+done
