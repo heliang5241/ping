b9e8ed4110154ebc218fcc0bea2c88f82f72b793
heliang5241 <1960050004@qq.com>
2016-12-29 08:49:22 +0800

    123

--- add_puppet_node.sh
+++ add_puppet_node.sh
@@ -7,8 +7,6 @@ cat ip.txt|grep -v ^#|grep -v ^$|while read line
 do
 ip3=`echo $line|awk -F. '{print$3}'`
 ip4=`echo $line|awk -F. '{print$4}'`
-#echo $ip3
-#echo $ip4
 node_name=`echo $1|awk -F. '{print$1}'`
 echo -e " node $node_name-$ip3-$ip4 inherits $node_name {\n\n\n}\n" >>$1
 done
